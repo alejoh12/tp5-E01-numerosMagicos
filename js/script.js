84979e44f4988e13b43994caf519cef372cc3445
Alejo Herrera <alejoherrera1166@gmail.com>
2023-11-21 03:40:40 -0300

    numero mágico creado.

--- js/script.js
+++ js/script.js
@@ -2,16 +2,19 @@
 1- Crea una web con bootstrap y js, que contenga un botón comenzar el juego, en ese momento se crea un número aleatorio que el usuario deberá adivinar, la interfaz del usuario debe tener además un input para ingresar un número y un botón enviar, al presionar el botón enviar mostrar en un alert si el usuario adivino o no el número mágico, si no lo adivino indicarle con un alert si el numero que ingreso es mayor o menor al número mágico.
 Cuando el usuario adivine el numero mostrar un mensaje indicando al usuario que adivino el numero.
 */
-const comenzar = () => {
-    console.log("Desde la función comenzar.");
+let numeroMagico = 0;
 
+const comenzar = () => {
     const etiquetaNueva = document.createElement("h3");
     etiquetaNueva.innerHTML = `Excelente! Ahora ya puedes adivinar el número mágico. 😁`;
     etiquetaNueva.className = `my-1 text-danger`;
 
     const seccionInformacionExtra = document.getElementsByClassName("articulo");
-    console.log(seccionInformacionExtra);
     seccionInformacionExtra[0].appendChild(etiquetaNueva);
+
+    numeroMagico = Math.round(Math.random()*(100));
+
+    console.log(numeroMagico);
 }
 
 const btnComenzar = document.getElementById("btnComenzar");
